d4786850b1dd4a78e8d23173df56fbcce936e6a0
washoe <b.oliver@optusnet.com.au>
2014-06-14 15:42:13 +1000

    FIX: no port = no colon in podcast url

--- public/js/main.js
+++ public/js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 		// request podcast xml doc from server using itpc protocol
 		var protocol = 'itpc';
 		//protocol = 'http';// for testing
-		var getpodcastUrl = protocol + '://'+location.hostname+":"+location.port+'/getpodcast/'+$(event.target).data('programId');
+		var getpodcastUrl = protocol + '://'+location.hostname+(location.port ? ':'+location.port : '')+'/getpodcast/'+$(event.target).data('programId');
 		window.open(getpodcastUrl);
 	})
 });
